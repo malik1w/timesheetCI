a29047fbc7bbeb72787c9a6e70aed8fcb6e5bd03
malik1w <mahmoudimelek95@gmail.com>
2021-02-23 12:10:11 +0100

    add log4j

--- src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/DepartementServiceImpl.java
@@ -1,10 +1,10 @@
 package tn.esprit.spring.services;
 
 import java.util.List;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.repository.DepartementRepository;
@@ -15,13 +15,24 @@ public class DepartementServiceImpl implements IDepartementService {
 	@Autowired
 	DepartementRepository deptRepository;
 
+	private static final Logger l = LogManager.getLogger(DepartementServiceImpl.class);
+
 	public List<Departement> getAllDepartements() {
-		return (List<Departement>) deptRepository.findAll();
+		l.info("In  retrieveAllDepts : "); 
+		List<Departement> listDept = (List<Departement>) deptRepository.findAll();
+		for (Departement deps : listDept) {
+			l.debug("deps +++ : " + deps);
+		}
+		l.info(" all deps retrieved");
+		return listDept;
 	}
 
 	@Override
 	public Departement addOrUpdateDep(Departement d) {
+		l.info("in add dept : "+ d);
 		Departement dept = deptRepository.save(d);
+		l.info(d+"added");
+
 		return dept;
 	}
 	
