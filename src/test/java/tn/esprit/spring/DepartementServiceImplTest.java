6311202bcddded5757059ab49328af53b0d1990a
malik1w <mahmoudimelek95@gmail.com>
2021-02-23 13:09:49 +0100

    jUnit Test

---
+++ src/test/java/tn/esprit/spring/DepartementServiceImplTest.java
@@ -0,0 +1,42 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tn.esprit.spring.entities.Departement;
+
+import tn.esprit.spring.services.DepartementServiceImpl;
+import tn.esprit.spring.services.IDepartementService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class DepartementServiceImplTest {
+
+	@Autowired 
+	IDepartementService deps; 
+	@Test
+	public void testGetAllDepartements() {
+		// if there are 2 departements in DB : 
+		assertEquals(2, deps.getAllDepartements().size());
+	}
+
+	@Test
+	public void testAddOrUpdateDep() {
+		Departement d = new Departement("FinanceTest"); 
+	    Departement addDept = deps.addOrUpdateDep(d);
+	    assertEquals(d.getName(), addDept.getName());	}
+
+	@Test
+	public void testDeleteDepartementById() {
+		Departement d = new Departement("FinanceTest"); 
+	    deps.addOrUpdateDep(d);
+	    assertNotNull(d);
+	    deps.deleteDepartementById(d.getId());
+	}
+
+}
