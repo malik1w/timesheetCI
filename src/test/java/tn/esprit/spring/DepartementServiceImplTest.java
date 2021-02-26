a8cf2633efd2b684424276313fd15ef80f6cca9e
malik1w <mahmoudimelek95@gmail.com>
2021-02-26 12:48:38 +0100

    fix unit test

--- src/test/java/tn/esprit/spring/DepartementServiceImplTest.java
+++ src/test/java/tn/esprit/spring/DepartementServiceImplTest.java
@@ -2,12 +2,13 @@ package tn.esprit.spring;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import tn.esprit.spring.entities.Departement;
+
 
 import tn.esprit.spring.services.DepartementServiceImpl;
 import tn.esprit.spring.services.IDepartementService;
