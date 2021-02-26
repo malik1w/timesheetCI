a32c84312a0cdbad8235f363261ff0f034a5682c
aymenguediche <aymen.guediche@esprit.tn>
2021-02-26 12:54:45 +0100

    fixe test unitaire

--- src/test/java/tn/esprit/spring/ContratRepositoryTest.java
+++ src/test/java/tn/esprit/spring/ContratRepositoryTest.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
